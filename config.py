2278d0ffc6b8cd152ab18fecf646a38959a6a8b0
Thiago Oliveira <thiago.oliveira@ssys.com.br>
2019-09-09 15:06:48 -0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+from backup import Backup 
+from restore import Restore
+import argparse
+
+
+# 1 Alerts
+# 2 Notifications
+# 3 Criar backup de permissoes
+# 4 Confirmar a criacao e links e variaveis dos paineis e dashboards
+
+def main():
+
+    argp = argparse.ArgumentParser(description=__doc__,)
+
+    argp.add_argument('-k', '--key', dest='key', type=str,
+                        help='bearer token', required=True)
+    argp.add_argument('-H', '--host', dest='host', type=str,
+                        help='URL Grafana', required=True)
+    argp.add_argument('-d', '--dir', dest='directory', type=str,
+                        help='Directory for backup or get restore', required=True)
+    argp.add_argument('-p', '--proxy', dest='proxy', type=str,
+                        help='Proxy for requests (host and port)', default= None, required=False)
+    type_group = argp.add_mutually_exclusive_group(required=True)
+    type_group.add_argument('-b', '--backup', action='store_true', dest='type',
+                        help='Backup panels and configs')
+    type_group.add_argument('-r', '--restore', action='store_false', dest='type',
+                        help='Restore panels and configs')
+
+    args = argp.parse_args()
+
+    proxies = {
+        "http": args.proxy,
+        "https": args.proxy
+    }
+
+    head = {
+        'Authorization': 'Bearer {}'.format(args.key),
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8'
+    }
+
+    API = '{}/api'.format(args.host)
+    folder = '{}/folders'.format(API)
+    dashboard = '{}/dashboards'.format(API)
+
+    attr = [args.directory, head, proxies, API, dashboard, folder]
+
+    if args.type:
+        bkp = Backup(*attr)
+        bkp.get_organization()
+        bkp.get_all_dashboards()
+        bkp.get_all_datasources()
+    else:
+        rst = Restore(*attr)
+        rst.post_all_dashboards()
+        rst.post_all_datasources()
+
+if __name__ == "__main__":
+    
+    
+    main()
+
+
+
+
+
+
+
+
---
+++ restore.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+from backup import Backup 
+from requests import get, post
+from json import loads
+from glob import glob
+from urllib import quote
+
+
+
+class Restore(Backup, object):
+    
+    def __init__(self, FILE_DIR, head, proxies, API, dashboard, folder):
+        super(Restore, self).__init__(FILE_DIR, head, proxies, API, dashboard, folder)
+
+    def post_folders(self, folderTitle):
+        
+        directory = '{}/{}/folders'.format(self.FILE_DIR,
+                                           self.get_organization())
+
+        with open('{}/{}.json'.format(directory,
+                    folderTitle.replace(' - ', ' ').replace(' ', '-').lower().encode('utf-8')),
+                        'r') as f:
+            raw = f.read()
+            data_raw = loads(raw)
+            data = {
+                'title': data_raw['title']
+            }
+            
+            try:
+                request = post(self.folder, headers=self.head, json=data, proxies=self.proxies)
+                post_response = request.json()
+            except:
+                folders = get('{}/search?query={}'.format(self.API,
+                                    quote(data['title'].encode("utf-8"))),
+                                    headers=self.head, proxies=self.proxies)          
+                post_response = folders.json()
+            data = post_response[0]['id']
+
+            return data
+
+    def post_all_dashboards(self):
+
+        directory = '{}/{}/dashboards'.format(self.FILE_DIR,
+                                              self.get_organization())
+
+        files = [f for f in glob("{}/*.json".format(directory))]
+
+        for file in files:
+            with open('{}'.format(file), 'r') as f:
+                raw = f.read()
+                data_raw = loads(raw)
+
+                data = {
+                    'folderId': self.post_folders(data_raw['meta']['folderTitle']) if data_raw['meta']['folderId'] != 0 else 0,
+                    'overwrite':True,
+                    'dashboard': {
+                        'tags': data_raw['dashboard']['tags'],
+                        'title': data_raw['dashboard']['title'],
+                        'version': data_raw['dashboard']['version'],
+                        'timezone': data_raw['dashboard']['timezone'],
+                        'schemaVersion': data_raw['dashboard']['schemaVersion'],
+                        'templating': data_raw['dashboard']['templating'],
+                        'id': '',
+                        'uid': ''
+                    }  
+                }
+
+                if 'rows' in data_raw['dashboard'].keys():
+                    data['dashboard'].update({'rows': data_raw['dashboard']['rows']})
+                else:
+                    data['dashboard'].update({'panels': data_raw['dashboard']['panels']})
+            
+                post('{}/db'.format(self.dashboard),
+                                        headers=self.head, json=data, proxies=self.proxies)
+
+    def post_all_datasources(self):
+        directory = '{}/{}/datasources'.format(self.FILE_DIR,
+                                               self.get_organization())
+
+        files = [f for f in glob("{}/*.json".format(directory))]
+
+        for file in files:
+            with open('{}'.format(file), 'r') as f:
+                raw = f.read()
+                data_raw = loads(raw)
+                post('{}/datasources'.format(self.API),
+                                                headers=self.head, json=data_raw, proxies=self.proxies)
