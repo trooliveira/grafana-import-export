6f48ba867709afcaeeff78727b6cc4f78c144d4e
Thiago Oliveira <thiago.oliveira@ssys.com.br>
2020-02-05 15:16:32 -0300

    source_2 archives

--- config.py
+++ config.py
@@ -1,57 +1,100 @@
 #!/usr/bin/env python
 
-from backup import Backup
-from restore import Restore
+from organizations import Organization
+from datasources import Datasource
+from dashboard_folder import Dashboard
+from pathvalidate.argparse import validate_filepath, ValidationError
 import argparse
+import sys
 
 
 def main():
 
-    argp = argparse.ArgumentParser(description=__doc__,)
+    glb = argparse.ArgumentParser(conflict_handler='resolve')
+    glb.add_argument('-H', '--host', dest='host', type=str,
+                    help='URL Grafana', required=True)
+    glb.add_argument('-P', '--port', dest='port', type=str,
+                    help='Port URL Grafana', required=True)
+    glb.add_argument('-d', '--dir', dest='directory', type=str,
+                    help='Directory for backup or get restore',
+                    required=True)
+    glb.add_argument('--proxy', dest='proxy', type=str,
+                    help='Proxy for requests (host and port)',
+                    default=None, required=False)
 
-    argp.add_argument('-k', '--key', dest='key', type=str,
-                      help='bearer token', required=True)
-    argp.add_argument('-H', '--host', dest='host', type=str,
-                      help='URL Grafana', required=True)
-    argp.add_argument('-d', '--dir', dest='directory', type=str,
-                      help='Directory for backup or get restore', required=True)
-    argp.add_argument('-p', '--proxy', dest='proxy', type=str,
-                      help='Proxy for requests (host and port)', default=None, required=False)
-    type_group = argp.add_mutually_exclusive_group(required=True)
-    type_group.add_argument('-b', '--backup', action='store_true', dest='type',
+    type_group = glb.add_mutually_exclusive_group(required=True)
+    type_group.add_argument('--backup', action='store_true', dest='type',
                             help='Backup panels and configs')
-    type_group.add_argument('-r', '--restore', action='store_false', dest='type',
+    type_group.add_argument('--restore', action='store_false', dest='type',
                             help='Restore panels and configs')
 
-    args = argp.parse_args()
+    type_form = glb.add_mutually_exclusive_group(required=True)
+    type_form.add_argument('--current', action='store_true', dest='form')
+    type_form.add_argument('--all', action='store_false', dest='form')
 
-    proxies = {
+    key_usr = glb.add_mutually_exclusive_group(required=True)
+    key_usr.add_argument('-k', '--api-key', dest='api_key',
+                     type=str, help='bearer token')
+    key_usr.add_argument('-u', '--user', dest='user', type=str,
+                     help='user')
+
+    glb.add_argument('-p', '--password', dest='passd', type=str,
+                     help='password')
+
+    args = glb.parse_args()
+
+    try:
+        validate_filepath(args.directory)
+    except ValidationError:
+        raise ValidationError, ValueError
+
+    host_port_request = '{}:{}'.format(args.host, args.port)
+    try:
+        host_request = 'http://api_key:{}@{}'.format(args.api_key, host_port_request)
+    except Exception:
+        try:
+            user_pass_request = '{}:{}'.format(args.user, args.passd)
+            host_request_basic_auth = 'http://{}@{}'.format(user_pass_request, host_port_request)
+        except ValueError as e:
+            raise 'It needs password arg', e
+
+    org_api = 'api/org'
+    orgs_api = 'api/orgs'
+    folder_api = 'api/folders'
+    dashboard_api = 'api/dashboards/db'
+    dashboard_search_api = 'api/search?type=dash-db&query=&'
+    folder_search_api = 'api/search?query'
+    datasource_api = 'api/datasources'
+    directory = args.directory
+    proxy = {
         "http": args.proxy,
         "https": args.proxy
     }
 
-    head = {
-        'Authorization': 'Bearer {}'.format(args.key),
-        'Content-Type': 'application/json; charset=utf-8',
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8'
-    }
+    if args.type is True:
+        if args.form is True:
+            Organization(directory, host_request, proxy, org_api).get_organization()
+        elif args.form is False:
+            Organization(directory, None, proxy, org_api,
+                         host_request_basic_auth, orgs_api).get_organization()
 
-    API = '{}/api'.format(args.host)
-    folder = '{}/folders'.format(API)
-    dashboard = '{}/dashboards'.format(API)
-
-    attr = [args.directory, head, proxies, API, dashboard, folder]
-
-    if args.type:
-        bkp = Backup(*attr)
-        bkp.get_organization()
-        bkp.get_all_dashboards()
-        bkp.get_all_datasources()
-    else:
-        rst = Restore(*attr)
-        rst.post_all_dashboards()
-        rst.post_all_datasources()
+        Datasource(directory, host_request, proxy,
+                   datasource_api).get_datasource()
+        Dashboard(directory, host_request, proxy,
+                  dashboard_api, dashboard_search_api,
+                  folder_api, folder_search_api).get_dashboard()
+    elif args.type is False:
+        if args.form is True:
+            Organization(directory, host_request, proxy,
+                         org_api).post_organization()
+        elif args.form is False:
+            Organization(directory, None, proxy, org_api,
+                         host_request_basic_auth, orgs_api).post_organization()
+        Datasource(directory, host_request, proxy,
+                   datasource_api).post_datasource()
+        Dashboard(directory, host_request, proxy,
+                  dashboard_api, dashboard_search_api,
+                  folder_api, folder_search_api).post_dashboard()
 
 
 if __name__ == "__main__":
